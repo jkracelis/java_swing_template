4a26977cdc625bf9dba20cdfbc3861f557e0bce3
cr3wcr3w <racelisjk@gmail.com>
2026-05-19 15:02:16 +0800

    chore: test github formatter workflow

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -5,7 +5,7 @@ import org.example.view.MainView;
 
 public class App {
 
-  public static void main(String[] args) {
+				  public static void main(String[] args) {
     SwingUtilities.invokeLater(App::startApp);
   }
 
